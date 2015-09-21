bc3339bda7ff03d9595e4e92f290c6261ede27af
nikpcali <nikpcali@gmail.com>
2015-09-20 17:15:09 -0700

    Initial Commit
    Java Re-entrant Locks

---
+++ src/App.java
@@ -0,0 +1,56 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		final Runner runner = new Runner();
+		
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				try {
+					runner.firstThread();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					runner.secondThread();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		runner.finished();
+		
+		
+
+	}
+
+}
